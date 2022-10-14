d08267f6b35ace1b97977b901c7ec1967ee7a631
Ivan <salata.vanya191@gmail.com>
2022-10-14 15:13:23 +0300

    1.0

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,13 @@
 (() => {
   const newLocal = "[data-modal-open]";
-    const refs = {
+  const refs = {
+      openModalBtnMobile: document.querySelector("[data-modal-open-mobile]"), 
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
-    };
+   };
   
+    refs.openModalBtnMobile.addEventListener("click", toggleModal);
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
